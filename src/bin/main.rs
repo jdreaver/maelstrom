aab0d9fd815e2c3bad82a8147de88b6ac9bfcd9d
David Reaver <me@davidreaver.com>
2023-04-08 14:57:10 -0700

    Process init message

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,5 @@
+use std::io::{BufRead, BufReader, Write};
+
 use color_eyre::eyre::{Result, WrapErr};
 
 use maelstrom::*;
@@ -5,19 +7,24 @@ use maelstrom::*;
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let msg = Message {
-        src: "c1".to_string(),
-        dest: "n1".to_string(),
-        body: Payload::Init(Init {
-            msg_id: 1,
-            node_id: "n1".to_string(),
-            node_ids: vec!["n1".to_string(), "n2".to_string()],
-        }),
-    };
+    let stdin = std::io::stdin();
+    let reader = BufReader::new(stdin.lock());
 
-    let msg_str = serde_json::to_string_pretty(&msg).wrap_err("failed to serialize")?;
+    let stdout = std::io::stdout();
+    let mut writer = stdout.lock();
 
-    println!("{}", msg_str);
+    let mut node = Node::new();
+
+    for line in reader.lines() {
+        // Read input from a stdin line
+        let line = line.wrap_err("failed to get stdin line")?;
+        let message: Message = serde_json::from_str(&line).wrap_err("failed deserializing Message")?;
+
+        // Process message and write to stdout line
+        let response = node.process_message(&message);
+        let serialized = serde_json::to_string(&response).unwrap();
+        writeln!(writer, "{}", serialized).unwrap();
+    }
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,41 @@ pub struct Init {
 pub struct InitOk {
     pub in_reply_to: usize,
 }
+
+#[derive(Debug)]
+pub struct Node {
+    pub node_id: String,
+    pub next_msg_id: usize,
+}
+
+impl Node {
+    pub fn new() -> Self {
+        Self {
+            node_id: "UNKNOWN_NODE".to_string(),
+            next_msg_id: 0,
+        }
+    }
+
+    pub fn process_message(&mut self, message: &Message) -> Message {
+        // msg_id is monotonically increasing
+        self.next_msg_id += 1;
+
+        match &message.body {
+            Payload::Init(Init {
+                msg_id,
+                node_id,
+                node_ids: _,
+            }) => {
+                self.node_id = node_id.clone();
+                Message {
+                    src: self.node_id.clone(),
+                    dest: message.src.clone(),
+                    body: Payload::InitOk(InitOk {
+                        in_reply_to: *msg_id,
+                    }),
+                }
+            }
+            Payload::InitOk(_) => panic!("didn't expect init_ok message"),
+        }
+    }
+}
